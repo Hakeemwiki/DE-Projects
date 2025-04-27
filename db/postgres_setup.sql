f72e4bc12e1261e20b998f39679b28fe9f75b518
Hakeemwiki <hakeem.wikireh@amalitechtraining.org>
2025-04-27 21:33:07 +0000

    feat(database): extend events table with user_name and user_email fields

--- db/postgres_setup.sql
+++ db/postgres_setup.sql
@@ -1,6 +1,8 @@
 CREATE TABLE IF NOT EXISTS events (
     event_id SERIAL PRIMARY KEY,
     user_id INT,
+    user_name VARCHAR(255),
+    user_email VARCHAR(255),
     event_type VARCHAR(60),
     product_id INT,
     product_name VARCHAR(150),
