5e13ca4bb58cafdd0ec1d3b20604d8362edfc5aa
Hakeemwiki <hakeem.wikireh@amalitechtraining.org>
2025-05-04 20:11:27 +0000

    feat(producer): add Kafka producer script with dynamic config loading from YAML

---
+++ scripts/kafka_producer.py
@@ -0,0 +1,19 @@
+
+# Importing required libraries
+import yaml
+import logging
+from kafka import KafkaProducer
+from data_generator import generate_heartbeat
+
+# Set up logging to see what's happening
+logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s] %(message)s")
+
+# Read the configuration from config.yaml
+with open("config.yml", "r") as file:
+    config = yaml.safe_load(file)
+
+# Get Kafka settings from the config file
+kafka_config = config["kafka"]
+bootstrap_servers = kafka_config["bootstrap_servers"] # localhost:9093
+topic = kafka_config["topic"] #heartbeats
+
