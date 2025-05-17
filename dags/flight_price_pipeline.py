7348e7d4158a65943660105779ad2c5cdc680f08
Hakeemwiki <hakeem.wikireh@amalitechtraining.org>
2025-05-17 17:52:40 +0000

    feat: add transformation and loading tasks to flight price pipeline DAG

--- dags/flight_price_pipeline.py
+++ dags/flight_price_pipeline.py
@@ -48,3 +48,31 @@ with DAG(
         },
         doc_md = "Validates data for nulls, types and inconsistencies"
     )
+
+    # Task 3: Transform and Compute KPIs
+    transform_task = PythonOperator(
+        task_id='transform_and_compute_kpis',
+        python_callable=transform_and_compute_kpis,
+        op_kwargs={
+            'mysql_conn_id': 'mysql_staging',
+            'table_name': 'flight_prices_raw'
+        }, 
+        provide_context=True, # Pass context to the function
+        doc_md = "Calculates Total Fare and computes KPIs using Seasonality."
+    )
+
+    # Task 4: Load to PostgreSQL
+    load_task = PythonOperator(
+        task_id='load_to_postgres',
+        python_callable=load_to_postgres,
+        op_kwargs={
+            'mysql_conn_id': 'mysql_staging',
+            'postgres_conn_id': 'postgres_analytics',
+            'mysql_table': 'flight_prices_raw_transformed',
+            'kpis': '{{ task_instance.xcom_pull(task_ids="transform_and_compute_kpis") }}' # Pull KPIs from the previous task
+        },
+        doc_md = "Loads transformed data and KPIs to PostgreSQL"
+    )
+
+    # Set task dependencies
+    ingest_task >> validate_task >> transform_task >> load_task
